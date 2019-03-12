ed522b21b53ffa5a19115d564f7a6e72f8f69343
Nikolai Timofeev <extezy34@gmail.com>
2019-03-12 22:39:48 +0300

    Class Programmer

---
+++ PycharmProjects/Yellow-Duck-Python/venv/include/Yellow-Duck/Объекты и классы/Programmer.py
@@ -0,0 +1,15 @@
+from Person import Person
+
+class Programmer(Person):
+
+    __prog_exp = 0
+
+    def set_prog_exp(self, px):
+        self.__prog_exp = px
+
+    def prog_exp(self):
+        return self.__prog_exp
+
+    def description_of_person(self):
+        super().description_of_person()
+        print('/ I am programmer and my experience ', self.__prog_exp)
--- PycharmProjects/Yellow-Duck-Python/venv/include/Yellow-Duck/Объекты и классы/main.py
+++ PycharmProjects/Yellow-Duck-Python/venv/include/Yellow-Duck/Объекты и классы/main.py
@@ -1,9 +1,10 @@
 from Person import Person
 from House import House
+from Programmer import *
 
 person1 = Person('Mike', 34)
-person2 = Person('John', 24)
-person3 = Person('Den', 27)
+person2 = Programmer('John', 24)
+
 house = House('1street')
 
 
@@ -12,6 +13,7 @@ house.settle_person(person2)
 
 person1.description_of_person()
 person2.description_of_person()
+person2.set_prog_exp(20)
 
 house.description_of_house()
 
